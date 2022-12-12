4a0928f448c4cef1dbca8b3c25a314a8546210d8
sundalei <sundalei1988@gmail.com>
2022-12-12 17:43:13 +0800

    web services

---
+++ webservice-demo/src/main/java/com/example/CountryEndPoint.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CountryEndPoint {
+
+    private static final String NAMESPACE_URI = "http://example.com";
+
+    private final CountryRepository countryRepository;
+
+    public CountryEndPoint(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+    @ResponsePayload
+    public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+        GetCountryResponse response = new GetCountryResponse();
+        response.setCountry(countryRepository.findCountry(request.getName()));
+        return response;
+    }
+}
